234783692b7699dfe308a8fdf204460e11dc288b
Shadrack Githua <githuashadrack89@gmail.com>
2022-07-21 21:35:34 +0300

    Update UI for insert order, add cart functionality

--- insert_order.php
+++ insert_order.php
@@ -14,6 +14,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> Insert Order</title>
+    <link rel="stylesheet" href="css/styleOrders.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     
@@ -37,7 +39,7 @@
        
 <div class="row"><!-- row Begin -->
     
-    <div class="col-lg-12"><!-- col-lg-12 Begin -->
+    <div class="col-lg-4"><!-- col-lg-12 Begin -->
         
         <div class="panel panel-default"><!-- panel panel-default Begin -->
             
@@ -62,12 +64,11 @@
                       
                       <div class="col-md-6"><!-- col-md-6 Begin -->
                           
-                          <select name="product" class="form-control"><!-- form-control Begin -->
+                          <select name="product"  class="form-control"><!-- form-control Begin -->
                               
                               <option selected disabled> Select product </option>
                               
                               <?php 
-                              
                               $get_product = "select * from products";
                               $run_product = mysqli_query($con,$get_product);
                               
@@ -78,7 +79,7 @@
                                   
                                   echo "
                                   
-                                  <option value='$product_id'> $product_title </option>
+                                  <option  value='$product_id'> $product_title </option>
                                   
                                   ";
                                   
@@ -94,11 +95,30 @@
                    
                    <div class="form-group"><!-- form-group Begin -->
                        
-                      <label class="col-md-3 control-label"> Product Quantity </label> 
+                      <label class="col-md-3 control-label">Quantity </label> 
                       
                       <div class="col-md-6"><!-- col-md-6 Begin -->
-                          
-                          <input name="product_quantity" type="text" class="form-control" required>
+                      <div class="input">
+                      <?php 
+                              
+                              $get_product = "select * from products where product_id ='$product_id'";
+                              $run_productx = mysqli_query($con,$get_product);
+                              
+                              while ($row_product=mysqli_fetch_array($run_productx)){
+                                  $p_quantity = $row_product['product_quantity'];                                
+                                  echo "<select class='btn btn-primary dropdown-toggle  quantity='1' name='product_qty' id='product_qty'> ";
+                                          $i=1; $p_quantity =$p_quantity;
+                                          while ($i <= $p_quantity  ){
+                                          echo "<option value=".$i.">".$i."</option>";
+                                             $i++;
+                                           }
+                                          echo "</select>";
+                                  
+                              }
+                              
+                              ?>
+                                        </div>
+                         
                           
                       </div><!-- col-md-6 Finish -->
                        
@@ -110,7 +130,7 @@
                       
                       <div class="col-md-6"><!-- col-md-6 Begin -->
                           
-                          <input name="submit" value="Insert Product" type="submit" class="btn btn-primary form-control">
+                          <input name="submit" value="Add to cart" type="submit" class="btn btn-primary form-control">
                           
                       </div><!-- col-md-6 Finish -->
                        
@@ -123,8 +143,172 @@
         </div><!-- canel panel-default Finish -->
         
     </div><!-- col-lg-12 Finish -->
-    
+    <div id="content"><!-- #content Begin -->
+    <div id="cart" class="col-md-8"><!-- col-md-9 Begin -->
+               
+               <div class="box"><!-- box Begin -->
+                   
+                   <form action="index.php?insert_order" method="post" enctype="multipart/form-data"><!-- form Begin -->
+                       
+                       <h1>Order Summary</h1>
+                       
+                      
+                       
+                       <p class="text-muted">You currently have 0 item(s) in your cart</p>
+                       
+                       <div class="table-responsive"><!-- table-responsive Begin -->
+                           
+                           <table class="table"><!-- table Begin -->
+                               
+                               <thead><!-- thead Begin -->
+                                   
+                                   <tr><!-- tr Begin -->
+                                       
+                                       <th colspan="2">Product</th>
+                                       <th>Quantity</th>
+                                       <th>Unit Price</th>
+                                       <th>Shipping County</th>
+                                       <th colspan="1">Delete</th>
+                                       <th colspan="2">Sub-Total</th>
+                                       
+                                   </tr><!-- tr Finish -->
+                                   
+                               </thead><!-- thead Finish -->
+                               
+                               <tbody><!-- tbody Begin -->
+                                  
+                                  <?php 
+                                   
+                                   $total = 0;
+                                   $select_cart = "select * from cart";
+                       
+                                   $run_cart = mysqli_query($con,$select_cart);
+                                   
+                                   $count = mysqli_num_rows($run_cart);
+                                   while($row_cart = mysqli_fetch_array($run_cart)){
+                                       
+                                     $pro_id = $row_cart['p_id'];
+                                       
+                                     $shipping_county = $row_cart['county'];
+                                       
+                                     $pro_qty = $row_cart['qty'];
+                                       
+                                       $get_products = "select * from products where product_id='$pro_id'";
+                                       
+                                       $run_products = mysqli_query($con,$get_products);
+                                       
+                                       while($row_products = mysqli_fetch_array($run_products)){
+                                           
+                                           $product_title = $row_products['product_title'];
+
+                                            $pro_sale_price = $row_products['product_sale']; 
+                                           
+                                           $product_img1 = $row_products['product_img1'];
+                                           
+                                           $only_price = $row_products['product_price'];
+                                           
+                                           $sub_total = $row_products['product_price']*$pro_qty;
+                                           
+                                           $total += $sub_total;
+                                           
+                                   ?>
+                                   
+                                   <tr><!-- tr Begin -->
+                                       
+                                       <td>
+                                           
+                                           <img class="img-responsive" src="product_images/<?php echo $product_img1; ?>" alt="Product 3a">
+                                           
+                                       </td>
+                                       
+                                       <td>
+                                           
+                                           <?php echo $product_title; ?>
+                                           
+                                       </td>
+                                       
+                                       <td>
+                                          
+                                           <?php echo $pro_qty; ?>
+                                           
+                                       </td>
+                                       
+                                       <td>
+                                           
+                                           <?php echo $only_price; ?>
+                                           
+                                       </td>
+                                       
+                                       <td>
+                                           
+                                           <?php echo $shipping_county; ?>
+                                           
+                                       </td>
+                                       
+                                       <td>
+                                           
+                                           <input type="checkbox" name="remove[]" value="<?php echo $pro_id; ?>">
+                                           
+                                       </td>
+                                       
+                                       <td>
+                                           
+                                           Ksh <?php echo $sub_total; ?>
+                                           
+                                       </td>
+                                       
+                                   </tr><!-- tr Finish -->
+                                   
+                                   <?php } } ?>
+                                   
+                               </tbody><!-- tbody Finish -->
+                               
+                               <tfoot><!-- tfoot Begin -->
+                                   
+                                   <tr><!-- tr Begin -->
+                                       
+                                       <th colspan="5">Total</th>
+                                       <th colspan="2">Ksh <?php echo $total; ?></th>
+                                       
+                                   </tr><!-- tr Finish -->
+                                   
+                               </tfoot><!-- tfoot Finish -->
+                               
+                           </table><!-- table Finish -->
+                           
+                       </div><!-- table-responsive Finish -->
+                       
+                       <div class="box-footer"><!-- box-footer Begin -->
+                           
+                           
+                           
+                           <div class="pull-right"><!-- pull-right Begin -->
+                               
+                               <button type="submit" name="update" value="Update Cart" class="btn btn-default"><!-- btn btn-default Begin -->
+                                   
+                                   <i class="fa fa-refresh"></i> Update Cart
+                                   
+                               </button><!-- btn btn-default Finish -->
+                               
+                               <a href="index.php?view_confirmedPayments" class="btn btn-primary">
+                                   
+                                   Proceed Checkout <i class="fa fa-chevron-right"></i>
+                                   
+                               </a>
+                               
+                           </div><!-- pull-right Finish -->
+                           
+                       </div><!-- box-footer Finish -->
+                       
+                   </form><!-- form Finish -->
+                   
+               </div><!-- box Finish -->
+               
+           </div><!-- col-md-9 Finish -->
+        </div>
 </div><!-- row Finish -->
+
+
    
     <script src="js/tinymce/tinymce.min.js"></script>
     <script>tinymce.init({ selector:'textarea'});</script>
@@ -136,37 +320,16 @@
 
 if(isset($_POST['submit'])){
     
-    $product_title = $_POST['product_title'];
-    $product_cat = $_POST['product_cat'];
-    $cat = $_POST['cat'];
-    $manufacturer_id = $_POST['manufacturer'];
-    $product_price = $_POST['product_price'];
-    $product_quantity = $_POST['product_quantity'];
-    $product_label = $_POST['product_label'];
-    $product_sale = $_POST['product_sale'];
-    $product_keywords = $_POST['product_keywords'];
-    $product_desc = $_POST['product_desc'];
+    $product_id = $_POST['product'];
+    $product_quantity = $_POST['product_qty'];
     
-    $product_img1 = $_FILES['product_img1']['name'];
-    $product_img2 = $_FILES['product_img2']['name'];
-    $product_img3 = $_FILES['product_img3']['name'];
-    
-    $temp_name1 = $_FILES['product_img1']['tmp_name'];
-    $temp_name2 = $_FILES['product_img2']['tmp_name'];
-    $temp_name3 = $_FILES['product_img3']['tmp_name'];
-    
-    move_uploaded_file($temp_name1,"product_images/$product_img1");
-    move_uploaded_file($temp_name2,"product_images/$product_img2");
-    move_uploaded_file($temp_name3,"product_images/$product_img3");
-    
-    $insert_product = "insert into products (p_cat_id,cat_id,manufacturer_id,date,product_title,product_img1,product_img2,product_img3,product_price,product_quantity,product_label,product_sale,product_keywords,product_desc) values ('$product_cat','$cat','$manufacturer_id',NOW(),'$product_title','$product_img1','$product_img2','$product_img3','$product_price','$product_quantity','$product_label','$product_sale','$product_keywords','$product_desc')";
+    $insert_product = "insert into cart (p_id,qty) values ('$product_id','$product_quantity')";
     
     $run_product = mysqli_query($con,$insert_product);
-    
     if($run_product){
         
         echo "<script>alert('Product has been inserted sucessfully')</script>";
-        echo "<script>window.open('index.php?view_products','_self')</script>";
+        echo "<script>window.open('index.php?insert_order','_self')</script>";
         
     }
     
